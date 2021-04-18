81dbded415b1c3616568263248a83d395e511c7f
Ben_Langlois <benlangloiss@gmail.com>
2021-04-17 20:10:00 -0400

    o lord jesus thats alot of files

---
+++ gatsby_portfolio/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+import SEO from '../components/seo'
+const About = () => (
+    <Layout>
+        <SEO title="About Me" />
+        <h1>About Me</h1>
+    </Layout>
+    )
+
+export default About;
