f81c80b006fffd748f412b640edcdddffffa0aa2
Ben_Langlois <benlangloiss@gmail.com>
2023-12-21 18:56:24 -0500

    changed logic around dates of projects, changed project objects, added cube project

--- src/projects.js
+++ src/projects.js
@@ -4,6 +4,8 @@ import noteapp from './images/projects/noteapp.png'
 import weatherapp from './images/projects/weatherapp.png'
 import photography from './images/projects/photography.png'
 import tempconvert from './images/projects/tempconvert.png'
+import cube from './images/projects/cube.png'
+
 import github from './images/socials/iconmonstr-github-3.svg'
 import figma from './images/socials/figma-svgrepo-com.svg'
 
@@ -18,6 +20,7 @@ import figma from './images/socials/figma-svgrepo-com.svg'
     alt: 'Portfolio Homepage',
     diff: 3,
     date: new Date('September 30, 2023 00:00:00'),
+    progress: false,
     links:[
       {
         type: 'Github',
@@ -43,6 +46,7 @@ const projectsObj = [
         alt: 'Portfolio Homepage',
         diff: 4,
         date: new Date('September 30, 2023 00:00:00'),
+        progress: false,
         links:[
         {
             type: 'Github Repo',
@@ -62,8 +66,9 @@ const projectsObj = [
         url: 'https://codepen.io/ben-langlois/details/JjJrJqK',
         img: noteapp,
         alt: 'Note App Dashboard',
-        diff: 2,
+        diff: 1,
         date: new Date('December 15, 2021 00:00:00'),
+        progress: false,
         links:[
         ],
         tags:[
@@ -79,6 +84,7 @@ const projectsObj = [
         alt: 'Weather App Dashboard',
         diff: 5,
         date: new Date('March 1, 2023 00:00:00'),
+        progress: false,
         links:[
         {
             type: 'Github Repo',
@@ -98,8 +104,9 @@ const projectsObj = [
         url: 'https://ben-langlois.github.io/Photography-Portfolio/',
         img: photography,
         alt: 'Photography Portfolio landing page',
-        diff: 3,
+        diff: 2,
         date: new Date('November 2, 2023 00:00:00'),
+        progress: false,
         links:[
         {
             type: 'Github Repo',
@@ -118,21 +125,40 @@ const projectsObj = [
         ]
     },
     {
-        name: 'Temp Converter',
-        desc: "A simple Temperature Converter made with React, Tailwind, and JQuery. The goal of this was to write zero CSS, really just an excuse to try out Tailwind.",
-        url: 'https://codepen.io/ben-langlois/details/jOdwMRz',
-        img: tempconvert,
-        alt: 'Temperature Codepen page',
-        diff: 2,
-        date: new Date('November 15, 2023 00:00:00'),
-        links:[
-        ],
-        tags: [
-        'React',
-        'SCSS',
-        'Tailwind',
-        'JQuery'
-        ]
+      name: 'Temp Converter',
+      desc: "A simple Temperature Converter made with React, Tailwind, and JQuery. The goal of this was to write zero CSS, really just an excuse to try out Tailwind.",
+      url: 'https://codepen.io/ben-langlois/details/jOdwMRz',
+      img: tempconvert,
+      alt: 'Temperature Codepen page',
+      diff: 2,
+      date: new Date('November 15, 2023 00:00:00'),
+      progress: false,
+      links:[
+      ],
+      tags: [
+      'React',
+      'SCSS',
+      'Tailwind',
+      'JQuery'
+      ]
+    },
+    {
+      name: 'A Cube',
+      desc: "An interactive, 3d sandbox made which renders and allows interaction with different 3d shapes the 3.js library",
+      url: 'https://codepen.io/ben-langlois/pen/zYegMzB?editors=0010',
+      img: cube,
+      alt: 'Cube Codepen page',
+      diff: 2,
+      date: new Date('January 1, 2024 00:00:00'),
+      progress: true,
+      links:[
+      ],
+      tags: [
+      'React',
+      '3.js',
+      'SCSS',
+      'JQuery'
+      ]
     }
 ]
 
